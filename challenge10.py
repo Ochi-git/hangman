53dd412c6b8f1752e624e05b3b11509438311a11
Ochi-git <tmr.fibo@gmail.com>
2020-05-23 15:42:02 +0900

    my first commit

---
+++ challenge10.py
@@ -0,0 +1,48 @@
+#hangman game
+import random
+
+def hangman():
+    list_hangman = ['cat', 'dog', 'alligator', 'bird', 'gorilla',
+                'giraffe', 'rat', 'mouse', 'panda', 'kangaroo']
+    num = len(list_hangman)
+    num_random = random.randint(0,num-1)
+    word = list_hangman[num_random]
+    wrong = 0
+    stages = ['',
+              '___________         ',
+              '|                   ',
+              '|                   ',
+              '|         |         ',
+              '|         0         ',
+              '|       ／|＼       ',
+              '|       ／ ＼       ',
+              '|                   '
+              ]
+    rletters = list(word)
+    board = ['_'] * len(word)
+    win = False
+    print('ハングマンへようこそ！')
+    while wrong < len(stages) -1:
+        print('\n')
+        msg = '１文字を予想してね'
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print(' '.join(board))
+        e = wrong + 1
+        print('\n'.join(stages[0:e]))
+        if '_' not in board:
+            print('あなたの勝ち！')
+            print(' '.join(board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(stages[0:wrong+1]))
+        print('あなたの負け!正解は{}'.format(word))
+
+
+hangman()
